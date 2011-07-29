3d38a3c8f1ea31f1484da557872182000637337e
Richard Chandler <richard.chandlers@gmail.com>
2011-07-29 12:30:00 -0400

    now works with partially observed Z

--- R/spatialNmix.R
+++ R/spatialNmix.R
@@ -10,94 +10,41 @@ e2dist1 <- function (x, y)
 
 
 
-sim.data<-function(N=30,T=5,sigma=.6,lam0=1.5,knownID=0,lc=10,buff=3,sslower=0,ssupper=15) {
 
-  #  lc <- 10
+
+
+
+
+sim.data <- function(N=25, T=5, sigma=0.5, lam0=1, knownID=0, lc=10,
+    buff=3, sslower=0, ssupper=15)
+{
     coords <- seq(sslower+buff, ssupper-buff, length=lc)
     X <- cbind(x=rep(coords, each=lc), y=rep(coords, times=lc))
- ng<- npts<-nrow(X)
+    npts <- nrow(X)
     # Home range centers
-    sx <- runif(N, sslower,ssupper)
-    sy <- runif(N, sslower,ssupper)
+    sx <- runif(N, sslower, ssupper)
+    sy <- runif(N, sslower, ssupper)
     S <- cbind(sx, sy)
     D <- e2dist1(S, X)
-# plot(X,pch=20)
-# points(S,pch=20,col="red")
- lam<- lam0*exp(-(D*D)/(2*sigma*sigma))
- zlst<-list()
- y<-matrix(NA,nrow=npts,ncol=T)
- for(i in 1:T){
- z<-rpois(prod(dim(D)),lam[1:length(lam)])
- z<-matrix(z,nrow=nrow(lam),ncol=ncol(lam),byrow=FALSE)
- zlst[[i]]<-z
- }
-ncaps<-matrix(NA,nrow=N,ncol=T)
-for(i in 1:T){
-ncaps[,i]<-apply(zlst[[i]],1,sum)
-}
-capind<- (1:N)[apply(ncaps,1,sum)>0]
-
-if(knownID>0){
-if(length(capind)<=knownID){
- cat("ERROR of a serious kind",fill=TRUE)
- gooseegg<-NULL
- return(gooseegg)
-}
-known.kp<-capind[1:knownID]
-unknown.kp<- is.na(match(1:N,known.kp))
-zknown<-list()
-for(i in 1:T){
- zknown[[i]]<-zlst[[i]][known.kp,]  # knownID x traps matrix
- y[,i]<-t(zlst[[i]][unknown.kp,])%*%rep(1,N-knownID)
- }
-}
-if(knownID==0){
-zknown=NULL
-for(i in 1:T){
- y[,i]<-t(zlst[[i]])%*%rep(1,N)
-}
-}
-
-if(knownID>0){
-zarr<-array(0,c(knownID,ng,T))
- for(i in 1:T){
- zarr[,,i]<- zknown[[i]]
-  }
-}
-else{
-zarr<-NULL
-}
-
-
-
-list(y=y,N=N,T=T,sigma=sigma,lam0=lam0,X=X,S=S,knownID=knownID,zknown=zknown,zarr=zarr)
-
-
-}
-
-
-
-
-
-
-
-
-
-sim.data2 <- function(N=25, T=5, sigma=0.5, lam0=0.5, nside=10) {
-    coords <- seq(3, 12, length=nside)
-    X <- cbind(x=rep(coords, each=nside), y=rep(coords, times=nside))
-    ng <- npts <- nrow(X)
-    # Home range centers
-    S <- cbind(runif(N, 0, 15), runif(N, 0, 15))
-    D <- e2dist1(S, X)
     lam <- lam0*exp(-(D*D)/(2*sigma*sigma))
-    y <- matrix(NA,nrow=npts,ncol=T)
-    Z <- array(NA, c(N,npts,T))
-    for(t in 1:T){
-        Z[,,t] <- rpois(prod(dim(D)),lam)
-        y[,t] <- colSums(Z[,,t])
+    Z <- array(NA, c(N, npts, T))
+    for(t in 1:T) {
+        Z[,,t] <- rpois(prod(dim(D)), lam)
     }
-    list(y=y,N=N,T=T,sigma=sigma,lam0=lam0,X=X,S=S, Z=Z)
+    y <- apply(Z, c(2,3), sum)
+    capind <- (1:N)[apply(Z>0, 1, any)]
+    if(length(capind) < knownID)
+        stop("try again. Some known guys were never caught")
+    Zknown <- NULL
+    if(knownID>0) {
+        known.kp <- capind[1:knownID]
+        Zknown <- Z[known.kp,,]
+    }
+
+    list(y=y, N=N, T=T, sigma=sigma, lam0=lam0, X=X, S=S, buff=buff,
+         sslower=sslower, ssupper=ssupper, knownID=knownID,
+         Z=Z, Zknown=Zknown)
+
 }
 
 
@@ -105,22 +52,41 @@ sim.data2 <- function(N=25, T=5, sigma=0.5, lam0=0.5, nside=10) {
 
 
 
-
-spNmix <- function(y, X, xlims=c(0,15), ylims=c(0,15), M=NULL, niters) {
+funcZ <- function(y, X, Zknown, M, niters, sslower=0, ssupper=15) {
 
     R <- nrow(y)
     T <- ncol(y)
-    S <- cbind(runif(M,xlims[1],xlims[2]),runif(M,ylims[1],ylims[2]))
-    D <- e2dist1(S,X)
-    theta <- runif(1,.25,1.25)
-    lam0 <- runif(1,.1,1)
+    S <- cbind(runif(M, sslower, ssupper), runif(M, sslower, ssupper))
+    D <- e2dist1(S, X)
+    theta <- runif(1, .25, 1.25)
+    lam0 <- runif(1, .1, 1)
     lam <- lam0*exp(-(D*D)/(2*theta*theta))
     w <- rbinom(M,1,.5)
     psi <- runif(1,.2,.8)
+
     Z <- array(NA, c(M,R,T))
+    nMarked <- 0
+    marked <- rep(FALSE, M)
+    if(!missing(Zknown) || !is.null(Zknown)) {
+        nMarked <- nrow(Zknown)
+        marked[1:nMarked] <- TRUE
+        Z[1:nMarked,,] <- Zknown
+    }
+    w[marked] <- 1
+    Zdata <- !is.na(Z)
     for(r in 1:R) {
         for(t in 1:T) {
-            Z[,r,t] <- rmultinom(1, y[r,t], lam[,r]*w)
+            if(y[r,t]==0) {
+                Z[,r,t] <- 0
+                next
+            }
+            unmarked <- !Zdata[,r,t]
+            nUnknown <- y[r,t] - sum(Z[!unmarked,r,t])
+            if(nUnknown < 0)
+                browser()
+            probs <- lam[,r]*w
+            probs <- probs[unmarked]
+            Z[unmarked,r,t] <- rmultinom(1, nUnknown, probs)
         }
     }
 
@@ -167,7 +133,7 @@ spNmix <- function(y, X, xlims=c(0,15), ylims=c(0,15), M=NULL, niters) {
         wUps <- 0
         seen <- apply(Z>0, 1, any)
         for(i in 1:M) {
-            if(seen[i])
+            if(seen[i] | marked[i])
                 next
             wcand <- ifelse(w[i]==0, 1, 0)
 
@@ -187,7 +153,15 @@ spNmix <- function(y, X, xlims=c(0,15), ylims=c(0,15), M=NULL, niters) {
             zip <- lam[,r]*w
             probs <- zip/sum(zip)
             for(t in 1:T) {
-                Z[,r,t] <- rmultinom(1, y[r,t], probs)
+                if(y[r,t]==0) {
+                    Z[,r,t] <- 0
+                    next
+                }
+                unmarked <- !Zdata[,r,t]
+                nUnknown <- y[r,t] - sum(Z[!unmarked,r,t])
+                if(nUnknown < 0)
+                    browser()
+                Z[unmarked,r,t] <- rmultinom(1, nUnknown, probs[unmarked])
             }
         }
 
@@ -198,15 +172,15 @@ spNmix <- function(y, X, xlims=c(0,15), ylims=c(0,15), M=NULL, niters) {
         # update S
         Sups <- 0
         for(i in 1:M) {   # note this is "M" in general
-            Scand <- c(rnorm(1, S[i,1], 0.5),
-                       rnorm(1, S[i,2], 0.5))
-            inbox <- Scand[1]>=xlims[1] & Scand[1]<=xlims[2] &
-                     Scand[2]>=ylims[1] & Scand[2]<=ylims[2]
+            Scand <- c(rnorm(1, S[i,1], 0.1),
+                       rnorm(1, S[i,2], 0.1))
+            inbox <- Scand[1]>=sslower & Scand[1]<=ssupper &
+                     Scand[2]>=sslower & Scand[2]<=ssupper
             if(inbox) {
                 dtmp <- sqrt((Scand[1] - X[,1])^2 + (Scand[2] - X[,2])^2)
                 lam.cand <- lam0*exp(-(dtmp*dtmp)/(2*theta*theta) )
 
-                #
+                # recycle lam over Z
                 ll <- sum(dpois(Z[i,,], lam[i,]*w[i], log=TRUE))
                 llcand <- sum(dpois(Z[i,,], lam.cand*w[i], log=TRUE))
 
@@ -235,9 +209,15 @@ spNmix <- function(y, X, xlims=c(0,15), ylims=c(0,15), M=NULL, niters) {
 
 
 
+
+
+
+
+
+
 # Example
 # library(coda)
-# str(sim1 <- sim.data(N=25, T=5, sigma=1, lam0=1))
-# fm1 <- funcZ(sim1$y, sim1$X, M=50, niters=6000)
+# str(sim1 <- sim.data(N=25, T=5, sigma=1, lam0=1, knownID=10))
+# fm1 <- funcZ(sim1$y, sim1$X, sim1$Zknown, M=50, niters=6000)
 # plot(mcmc(fm1))
 # rejectionRate(window(mcmc(fm1), start=3001))
