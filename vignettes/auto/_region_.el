f986f598cd3e82414b842aa9c0e3345a6dc757ec
Richard Chandler <richard.chandlers@gmail.com>
2012-09-07 15:04:09 -0400

    say wha

---
+++ vignettes/auto/_region_.el
@@ -0,0 +1,19 @@
+(TeX-add-style-hook "_region_"
+ (lambda ()
+    (TeX-run-style-hooks
+     "geometry"
+     "2cm}"
+     "color"
+     "verbatim"
+     "fullpage"
+     "natbib"
+     "authoryear"
+     "round"
+     "Sweave"
+     "fontenc"
+     "OT1"
+     "latex2e"
+     "art10"
+     "article"
+     "a4paper")))
+
