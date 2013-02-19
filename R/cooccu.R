6362d069a7a69cb073ad929c916e9bc39b10926b
Richard Chandler <richard.chandlers@gmail.com>
2013-02-19 09:33:57 -0500

    added cooccu.R

---
+++ R/cooccu.R
@@ -0,0 +1,13 @@
+cooccu <- function(psiformulaA = ~1, psiformulaB = ~1, dependformula = ~1,
+                   pformulaA = ~1, pformulaB = ~1,
+                   fpformulaA = NULL, fpformulaB = NULL,
+                   data,
+                   interaction=c("general", "dominance"),
+                   fpmodel=c("full", "confusion"),
+                   starts, method="BFGS", se=TRUE, ...) {
+
+
+
+
+}
+
